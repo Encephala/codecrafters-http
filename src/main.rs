d0e0760d5703a06a4f37a0322d1704f75e90d3d0
Encephala <jonathan_rietveld@hotmail.com>
2024-06-23 00:07:50 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::Read, io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -6,7 +6,18 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let mut buffer = [0; 64];
+
+                stream.read(&mut buffer).unwrap();
+
+                match std::str::from_utf8(&buffer).unwrap() {
+                    message if message.starts_with("GET / ") => {
+                        stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                    },
+                    _ => {
+                        stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                    },
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
